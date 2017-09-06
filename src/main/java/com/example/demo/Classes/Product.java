ca9e98e8b74513be7957f0f0200dfda58c0ab1db
Martin <martin_prevorsek@hotmail.com>
2017-09-06 13:33:42 +0200

    modified printProductList

--- src/main/java/com/example/demo/Classes/Product.java
+++ src/main/java/com/example/demo/Classes/Product.java
@@ -1,16 +1,35 @@
 package com.example.demo.Classes;
 
 
+import com.example.demo.Repos.ProdRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
 
     public int product_id;
     public String product_name;
     public double product_price;
+    public String product_color;
+    public int product_size;
+    public int product_stock;
+    public double product_weight;
+    public String product_image;
+    public String product_description;
 
 
-    public Product(int product_id, String product_name, double product_price) {
+    public Product(int product_id, String product_name, double product_price, String product_color, int product_size, int product_stock, double product_weight, String product_image, String product_description) {
         this.product_id = product_id;
         this.product_name = product_name;
         this.product_price = product_price;
+        this.product_color = product_color;
+        this.product_size = product_size;
+        this.product_stock = product_stock;
+        this.product_weight = product_weight;
+        this.product_image = product_image;
+        this.product_description = product_description;
+
     }
+
 }
--- src/main/java/com/example/demo/Repos/DbRepo.java
+++ src/main/java/com/example/demo/Repos/DbRepo.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +16,12 @@ public class DbRepo implements ProdRepo {
     DataSource dataSource;
 
     @Override
-    public List<Product> printProductList() {
+    public List<Product> getProductList() {
         try (Connection conn = dataSource.getConnection();
              Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT Product_id, Product_name, Product_price FROM Products")) {
+             ResultSet rs = stmt.executeQuery("SELECT product_id, product_name, " +
+                     "product_price, product_color, product_size, stock, product_weight, product_image, " +
+                     "product_description FROM Products")) {
             List<Product> productList = new ArrayList<>();
             while (rs.next()) productList.add(rsProduct(rs));
             return productList;
@@ -32,10 +31,14 @@ public class DbRepo implements ProdRepo {
     }
 
     private Product rsProduct(ResultSet rs) throws SQLException {
-        return new Product(rs.getInt("Product_id"),
-                rs.getString("Product_name"),
-                rs.getDouble("Product_price"));
+        return new Product(rs.getInt("product_id"),
+                rs.getString("product_name"),
+                rs.getDouble("product_price"),
+                rs.getString("product_color"),
+                rs.getInt("product_size"),
+                rs.getInt("stock"),
+                rs.getDouble("product_weight"),
+                rs.getString("product_image"),
+                rs.getString("product_description"));
     }
-
-
 }
